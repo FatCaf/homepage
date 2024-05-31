341b904d175e7589dccc96949f4f0d93552aaffc
Mykyta Neklesa <nikitaneklesa5@gmail.com>
2024-05-31 20:15:51 +0300

    add interactive timeline

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import Timeline from './timeline/Timeline.js';
+
+const experiences = document.getElementById('experiences');
+const timeline = new Timeline();
+console.log(timeline);
+experiences.appendChild(timeline);
---
+++ scripts/timeline/layout.js
@@ -0,0 +1,60 @@
+export const layout = `<div class='timeline-wrapper'>
+    <h3>Interactive Timeline</h3>
+    <div class='timeline'>
+                <div class='timeline-point left'>|</div>
+                <div class='timeline-point mid'>|</div>
+                <div class='timeline-point right'>|</div>
+        <div class='timeline-item first'>
+            <div class='timeline-item-inner first' id='cs'>
+                <div class='details-wrapper'>
+                    <div class='details' id='details'>
+                    <p>SH++ IT School
+                    Computer Science course
+                    From: march 2023 <br />
+                    To: september 2023
+                    </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class='timeline-item second'>
+            <div class='timeline-item-inner second' id='front'>
+                <div class='details-wrapper'>
+                    <div class='details' id='details'>
+                    <p>SH++ IT School
+                    Front-end course <br />
+                    From: october 2023 <br />
+                    To: march 2024</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class='timeline-item third'>
+            <div class='timeline-item-inner third' id='front'>
+                <div class='details-wrapper'>
+                    <div class='details' id='details'>
+                    <p>SH++ IT School<br />
+                    K-POINTS <br />
+                    From: january 2024 <br />
+                    To: march 2024</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    
+        <div class='timeline-item fourth'>
+            <div class='timeline-item-inner fourth' id='front'>
+                <div class='details-wrapper'>
+                    <div class='details' id='details'>
+                        <p>SH++ IT School
+                        Snapshot-IT <br />
+                        From: april 2024 <br />
+                        To: may 2024</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`;
